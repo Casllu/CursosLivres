1e61e9693193c602e94b8b41ac12833c6aae8798
Lucas <lalmeida464@gmail.com>
2025-09-13 13:02:09 -0300

    first commit

---
+++ src/main/java/com/estagio/cursosLivres/CursosLivresApplication.java
@@ -0,0 +1,13 @@
+package com.estagio.cursosLivres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosLivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosLivresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estagio/cursosLivres/CursosLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estagio.cursosLivres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
