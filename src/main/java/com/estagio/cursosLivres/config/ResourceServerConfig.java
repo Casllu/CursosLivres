9450682cc0c4aaf18fa25896dece167bc53ccb28
Lucas Almeida <lalmeida464@gmail.com>
2025-10-31 12:09:32 -0300

    feat: Implementação do profile de teste para apresentação do projeto.

--- src/main/java/com/estagio/cursosLivres/config/ResourceServerConfig.java
+++ src/main/java/com/estagio/cursosLivres/config/ResourceServerConfig.java
@@ -1,9 +1,11 @@
 package com.estagio.cursosLivres.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
@@ -27,6 +29,15 @@ public class ResourceServerConfig {
 
 	@Value("${cors.origins}")
 	private String corsOrigins;
+
+	@Bean
+	@Profile("test")
+	@Order(1)
+	SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+		http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable());
+		http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+		return http.build();
+	}
 
 	@Bean
 	@Order(2)
